72ee49296a4f5f98f89f16e35433982e1f3ba40d
Backup and Recovery Manager for PostgreSQL <barman@backup.namos.local>
2013-04-19 20:03:17 +0400

    All good people

--- kissobak.sh
+++ kissobak.sh
@@ -4,24 +4,30 @@
  exit 1
 }
 
+me=$(readlink -e "$0")
 declare -A slf=(
 	[NAME]=${0##*/}
-	[PATH]=$(dirname $(readlink -e "$0"))
+	[PATH]=${0%/*}
+	[REAL]=$me
+	[REAL_PATH]=${me%/*}
+	[REAL_NAME]=${me##*/}
 )
 
-while getopts 'TDR' k; do
+DEBUG_LEVEL=info
+LOG_FILE='/var/log/postgresql/backup.log'
+while getopts 'TxR d: l:' k; do
  case $k in
   T) flTestOut='| cat -' ;;
-  D) set -x 		 ;;
+  x) set -x 		 ;;
   R) flReplicaMode=1 	 ;;
+  d) DEBUG_LEVEL="$OPTARG" ;;
+  l) LOG_FILE="$OPTARG"  ;;
   *) : 			 ;;
  esac
 done
 shift $((OPTIND-1))
-
 WHAT2DO=${1:-base}
-LOG_FILE='/var/log/postgresql/backup.log'
-DEBUG_LEVEL=info
+
 source /opt/scripts/functions/config.func
 { source /opt/scripts/functions/debug.func && \
   log_open "$LOG_FILE"; } || \
@@ -37,45 +43,57 @@ eval "$(read_ini $OurConfig)"
 if [[ -d ${OurConfig%/*}/clients ]]; then
  while read f; do
   [[ -r $f ]] || continue
-  eval "$(read_ini $f)"  
+  tmp_=$(mktemp /tmp/XXXXXXXXXX)
+  eval "$(read_ini $f)" 2>$tmp_ || error_ "Some error while reading $f ini file: $(<${tmp_})"
+  rm -f "$tmp_"
  done < <(find ${OurConfig%/*}/clients -type f -name '*.ini')
 fi
 
-source "${slf[PATH]}/${slf[NAME]%.*}.inc"
+{ source "${slf[REAL]%.*}.inc" || source "${0%.*}.inc"; }  || fatal_ 'Cant find my include file containing important functions'
 
 case $WHAT2DO in
-base)
+base) 
  [[ $2 ]] || { error_ 'We dont know, from where and what to copy'; exit 1; }
+ info_ 'Starting base backup...'
  if [[ $2 =~ \@ ]]; then 
   IFS='@:' read User RemoteHost Dir2Copy <<<"$2"
  else
   RemoteHost="$2"
-  eval "User=\${INI$RemoteHost[login_from_server]}; Dir2Copy=\${INI$RemoteHost[pgdata]}"
- fi 
+  eval "User=\${INI$RemoteHost[login_from_server]}; Dir2Copy=\${INI$RemoteHost[pgdata]}; PSQL=\${INI$RemoteHost[psql]:-psql}"
+ fi
+ debug_ "User=$User, Dir2Copy=$Dir2Copy, PSQL=$PSQL"
  TS="$(date +%Y%m%d_%H%M%S)"
- info_ "We requested to get base copy from $RemoteHost, BackupID: $TS"
+ info_ "We was requested to get base copy from $RemoteHost, BackupID: $TS"
  OurDestPath="${INIserver[backup_path]}/$RemoteHost/base/$TS"
  ${flTestOut+echo }mkdir -p "$OurDestPath"
  info_ "We was requested to get base copy from $RemoteHost, $Dir2Copy. We are going to place this copy in $OurDestPath"
- ${flTestOut+echo }ssh $User@$RemoteHost "psql <<<\"SELECT pg_start_backup('$TS', true);\""
+ trap 'pg_backup stop' SIGINT SIGTERM SIGHUP
+ pg_backup start || {
+  error_ "Coudnot start backup on $RemoteHost. Error=${ERR_MSG}"
+  exit 1
+ }
  _errc="$(${flTestOut+echo }rsync -a --exclude-from=${OurConfig%/*}/exclude.lst $User@$RemoteHost:${Dir2Copy%/}/ $OurDestPath 2>&1 >/dev/null)"
  (( $? )) && error_ "Some problem while RSYNC, description given: ${_errc}"
- ${flTestOut+echo }ssh $User@$RemoteHost "psql <<<'SELECT pg_stop_backup();'" 
-  eval "LoginAs=\${INI$RemoteHost[login_to_server]-$(whoami)}"
-  if [[ $flReplicaMode ]]; then
-   eval "cat <<EOF ${flTestOut->$OurDestPath/recovery.conf}
+ pg_backup stop || {
+  error_ "Some problem occured while stopping backup on $RemoteHost. Error=${ERR_MSG}"
+  exit 1
+ }
+ trap "" SIGINT SIGTERM SIGHUP
+ eval "LoginAs=\${INI$RemoteHost[login_to_server]-$(whoami)}"
+ if [[ $flReplicaMode ]]; then
+  eval "cat <<EOF ${flTestOut->$OurDestPath/recovery.conf}
 standby_mode = 'on'
 trigger_file = '/tmp/postgresql.trigger.5432'
 restore_command = 'scp ${LoginAs}@${INIserver[hostname]}:${INIserver[backup_path]}/$RemoteHost/wals/%f %p'
 archive_cleanup_command = '${INIserver[archive_cleanup_command]-/opt/PostgreSQL/current/bin/pg_archivecleanup} ${INIserver[backup_path]}/$RemoteHost/wals %r'
 EOF
 "
-  else
-   eval "cat <<EOF ${flTestOut->$OurDestPath/recovery.conf}
+ else
+  eval "cat <<EOF ${flTestOut->$OurDestPath/recovery.conf}
 restore_command = 'scp ${LoginAs}@${INIserver[hostname]}:${INIserver[backup_path]}/$RemoteHost/wals/%f %p'
 EOF
 "   
-  fi
+ fi
 ;;
 save_xlog)
  walsFile="$2"
